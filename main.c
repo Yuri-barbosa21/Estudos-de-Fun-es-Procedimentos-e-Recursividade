de0bf44fbcbb77f5d31cc3bd04112d4eb610bd2d
Yuri Barbosa <62626768+Yuri-barbosa21@users.noreply.github.com>
2022-04-17 00:50:40 +0000

    Funções para retornar soma de coluna e linha de uma matriz

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
 
 
 
@@ -42,6 +44,40 @@ void Imprimir(int id){
 //__________________________________________________________________________
 
 
+
+//--------- FUNÇÃO QUE RETORNA A SOMA DE UMA LINHA E COLUNA DA MATRIZ ----------
+int tam = 5; // Varialvel global
+void ImprimirMatriz(int matriz[5][5]){
+    int i, j;
+    for(i = 0; i < tam; i++){
+        for(j = 0; j < tam; j++){
+            printf("%2d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int SomarLinhaMatriz(int matriz[5][5], int linha){
+    int coluna, soma = 0;
+
+    for (coluna = 0; coluna < tam; coluna++){
+        soma += matriz[linha][coluna]; 
+    }
+    
+    return soma;
+}
+
+int SomarColunaMatriz(int matriz[5][5], int coluna){
+    int linha, soma = 0;
+
+    for (linha = 0; linha < tam; linha++){
+        soma += matriz[linha][coluna]; 
+    }
+    
+    return soma;
+}
+
+
 int main(void) {
 
   //--------------- FUNÇÃO QUE RETONRNA O TAMANHO DE UMA STRING --------------
@@ -77,7 +113,29 @@ int main(void) {
 //___________________________________________________________________________
 
 
+    //-------- FUNÇÃO QUE RETORNA A SOMA DE UMA LINHA E COLUNA DA MATRIZ ---------
+    int mat[5][5];
+    int i, j;
+
+    srand(time(NULL));
     
+    for(i = 0; i < tam; i++){
+        for(j = 0; j < tam; j++){
+            mat[i][j] = rand() % 10;
+        }
+    }
+
+    ImprimirMatriz(mat);
+
+    for(i = 0; i < tam; i++){
+        printf("Soma da linha %d: %d\n", i,  SomarLinhaMatriz(mat, i));
+    }
+
+    printf("\n");
+    
+    for(i = 0; i < tam; i++){
+        printf("Soma da linha %d: %d\n", i,  SomarColunaMatriz(mat, i));
+    }
     
   return 0;
 }
